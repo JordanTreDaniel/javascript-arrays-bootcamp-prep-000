c34612c2d6ede670038cad4ac17a846d1acb138d
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-05-23 14:07:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, el) {
+  var copy = [...arr];
+  copy.unshift(el);
+  return copy;
+}
+
+
+var destructivelyAddElementToBeginningOfArray = (arr, el) => {
+  arr.unshift(el);
+  return arr;
+}
+
+var addElementToEndOfArray = (arr, el) => {
+  var copy = [...arr];
+  copy.push(el);
+  return copy;
+}
+
+var destructivelyAddElementToEndOfArray = (arr, el) => {
+  arr.push(el);
+  return arr;
+}
+
+function accessElementInArray(arr, i) {
+  return arr[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function 
